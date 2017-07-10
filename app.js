66ab4f1552990eb35e07a66467101f8cb2fb18e3
Mercedes <mercedesmp1305@gmail.com>
2017-07-10 15:11:31 -0300

    sigue sin funcionar -.-

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 //incluimos dependencia
-var express = require ("express"),
-   app = express(),
-   bodyParser = require("body-parser"),
-   methodOverride = require ("method-override"),
-   mongoose = require('mongoose');
-var model = require('./models/tvshow')
-//importamos controllers
-var tvshowCtrl = require('./controllers/tvshows');
+var express  = require("express"),  
+    app      = express(),
+    http     = require("http"),
+    server   = http.createServer(app),
+    mongoose = require('mongoose');
 
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.json());  
 app.use(methodOverride());
 
-//API routes
-var router = express.Router();
 
-router.get('/', function(req, res) {
-   res.send("Hello word!");
-   next();
+app.get('/', function(req, res) {
+  res.send("Hello world!");
 });
 
+//API routes
+routes = require('./routes/tvshows')(app);
+/*
 router.route('/tvshows')
    .get(tvshowCtrl.findAllTVShows)
    .post(tvshowCtrl.addTVShow);
@@ -31,14 +27,15 @@ router.route('/tvshows/:id')
    .delete(tvshowCtrl.deleteTVShow);
 
 app.use('/api', router);
-
+*/
 //definimos puerto a usar
-mongoose.connect('mongodb://localhost:27017/tvshows', function(err, res){
-   if (err) {
-      console.log('ERROR: conectando a la base de datos '+ err);
-   }
-   app.listen(3000, function(){
-      console.log("Node server running on 3000");
-      console.log("connectado!")
-   })
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+	if(err) {
+		console.log('ERROR: connecting to Database. ' + err);
+	} else {
+		console.log('Connected to Database');
+	}
+});
+server.listen(8080, function() {
+  console.log("Node server running on http://localhost:8080");
 });
